8e34982a78f1fb88e8135e1a0179b74893c1073a
Valentijn Scholten <valentijnscholten@gmail.com>
2024-12-23 20:27:27 +0100

    Compose Metadata Analyzer: Refactor to support V2
    Signed-off-by: Valentijn Scholten <valentijnscholten@gmail.com>

--- src/main/java/org/dependencytrack/tasks/repositories/ComposerMetaAnalyzer.java
+++ src/main/java/org/dependencytrack/tasks/repositories/ComposerMetaAnalyzer.java
@@ -127,9 +127,9 @@ public class ComposerMetaAnalyzer extends AbstractMetaAnalyzer {
                 // According to https://github.com/composer/composer/blob/fb397acaa0648ba2668893e4b786af6465a41696/doc/05-repositories.md?plain=1#L197
                 // available-packages should contain ALL the packages in the repo.
                 // But in the Composer implementation the patterns are consulted even if available-packages is present and doesn't contain the package
-                // if (!repoRoot.has("available-package-patterns")) {
+                if (!repoRoot.has("available-package-patterns")) {
                     return meta;
-                // }
+                }
             }
         }
 
